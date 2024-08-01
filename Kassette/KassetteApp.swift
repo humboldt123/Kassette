772f12d195a4a87ed9cd939eba6f43960174d3bd
Vish <38266782+humboldt123@users.noreply.github.com>
2024-08-01 12:05:34 -0400

    Initial Commit

---
+++ Kassette/KassetteApp.swift
@@ -0,0 +1,17 @@
+//
+//  KassetteApp.swift
+//  Kassette
+//
+//  Created by Vish on 8/1/24.
+//
+
+import SwiftUI
+
+@main
+struct KassetteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
